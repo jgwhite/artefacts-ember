e5da25986aa96a1b3a4a05bb74e50a26bbe82cbd
Jamie White <jamie@jgwhite.co.uk>
2015-08-07 14:15:12 +0100

    ember install emberfire

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'artefacts',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://artefacts.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
