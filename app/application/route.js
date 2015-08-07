a9b2e7b17b87a3f003921c7fc71f44772be3e803
Jamie White <jamie@jgwhite.co.uk>
2015-08-07 15:27:39 +0100

    Support sign-out

--- app/application/route.js
+++ app/application/route.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
+const { inject } = Ember;
 
 export default Ember.Route.extend({
+  firebase: inject.service(),
+
+  actions: {
+    signOut() {
+      let firebase = this.get('firebase');
+
+      firebase.unauth();
+
+      this.transitionTo('sign-in');
+    }
+  }
 });
